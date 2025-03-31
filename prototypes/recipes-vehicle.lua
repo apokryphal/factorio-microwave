e8a84607daf868f6a35e8ff83c31c0dd67f9b808
apokryphal <20584067+apokryphal@users.noreply.github.com>
2025-03-31 08:54:31 -0700

    Consistency + remove superfluous whitespace

--- prototypes/recipes-vehicle.lua
+++ prototypes/recipes-vehicle.lua
@@ -4,23 +4,27 @@ data:extend({
 		name = "microwave-vehicle-receiver-small",
 		enabled = false,
 		energy_required = 3,
-		ingredients = { 
-			{type = "item", name = "plastic-bar", amount = 4}, 
-			{type = "item", name = "processing-unit", amount = 2}, 
-			{type = "item", name = "microwave-receiver-small", amount = 1} 
+		ingredients = {
+			{ type = "item", name = "plastic-bar", amount = 4 },
+			{ type = "item", name = "processing-unit", amount = 2 },
+			{ type = "item", name = "microwave-receiver-small", amount = 1 }
 		},
-		results={{type="item", name="microwave-vehicle-receiver-small", amount=1}},
+		results = {
+			{ type="item", name="microwave-vehicle-receiver-small", amount=1 }
+		},
 	},
 	{
 		type = "recipe",
 		name = "microwave-vehicle-receiver-large",
 		enabled = false,
 		energy_required = 4,
-		ingredients = { 
-			{type = "item", name = "plastic-bar", amount = 8}, 
-			{type = "item", name = "processing-unit", amount = 4}, 
-			{type = "item", name = "microwave-receiver-large", amount = 1} 
+		ingredients = {
+			{ type = "item", name = "plastic-bar", amount = 8 },
+			{ type = "item", name = "processing-unit", amount = 4 },
+			{ type = "item", name = "microwave-receiver-large", amount = 1 }
 		},
-		results={{type="item", name="microwave-vehicle-receiver-large", amount=1}},		
+		results = {
+			{ type="item", name="microwave-vehicle-receiver-large", amount=1 }
+		},
 	}
 })
