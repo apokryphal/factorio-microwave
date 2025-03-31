af7f2a35059dbc3969e3c8cf488f92a9af63ccfb
apokryphal <20584067+apokryphal@users.noreply.github.com>
2025-03-31 08:19:40 -0700

    Updated to live mod + indentation fixes

--- config.lua
+++ config.lua
@@ -1,5 +1,20 @@
-equipmentCategories = { "armor" }
+local function contains(table, element)
+	for _, value in pairs(table) do
+		if value == element then
+			return true
+		end
+	end
+	return false
+end
 
-for k,v in pairs(data.raw["equipment-category"]) do
-	table.insert(equipmentCategories, k)
+function getEquipmentCategories()
+	local equipmentCategories = { "armor" }
+
+	for k,v in pairs(data.raw["equipment-category"]) do
+		if not contains(equipmentCategories, k) then
+			table.insert(equipmentCategories, k)
+		end
+	end
+	
+	return equipmentCategories
 end
