50ba1a644b5f97bae16f680255246b9c0c701bfc
apokryphal <20584067+apokryphal@users.noreply.github.com>
2025-03-31 08:00:01 -0700

    Added t3 rx item + indent fixes

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -1,3 +1,10 @@
+local icon_base = "__camedo-microwave__/graphics/icons/"
+local icon_mw_tower_1 = icon_base.."tower.png"
+local icon_mw_rx_1 = icon_base.."microwave1.png"
+local icon_mw_rx_2 = icon_base.."microwave2.png"
+local icon_mw_rx_3 = icon_base.."microwave3.png"
+
+
 ----------------------------------------------------------------------------------------------------------------
 -- Microwave Items
 ----------------------------------------------------------------------------------------------------------------
@@ -5,32 +12,41 @@ data:extend({
 	{	-- Microwave-Tower
 		type = "item",
 		name = "microwave-tower",
-		icon = "__camedo-microwave__/graphics/icons/tower.png",
+		icon = icon_mw_tower_1,
 		icon_size = 32,
 		subgroup = "energy-pipe-distribution",
 		order = "a[energy]-c[big-electric-pole]-a",
 		place_result = "microwave-tower",
 		stack_size = 50
 	},	
-    {   -- Microwave-Receiver-Small
-        type = "item",
-        name = "microwave-receiver-small",
-        icon = "__camedo-microwave__/graphics/icons/microwave1.png",
-        icon_size = 32,
-        placed_as_equipment_result = "microwave-receiver-small",
-        subgroup = "equipment",
-        order = "a[energy-source]-a[solar-panel]-b",
-        stack_size = 100,
-    },
-    {   -- Microwave-Receiver-Large
-        type = "item",
-        name = "microwave-receiver-large",
-        icon = "__camedo-microwave__/graphics/icons/microwave2.png",
-        icon_size = 32,
-        placed_as_equipment_result = "microwave-receiver-large",
-        subgroup = "equipment",
-        order = "a[energy-source]-a[solar-panel]-c",
-        stack_size = 50,
-    }
+	{	-- Microwave-Receiver-Small
+		type = "item",
+		name = "microwave-receiver-small",
+		icon = icon_mw_rx_1,
+		icon_size = 32,
+		place_as_equipment_result = "microwave-receiver-small",
+		subgroup = "equipment",
+		order = "a[energy-source]-a[solar-panel]-b",
+		stack_size = 100,
+	},
+	{	-- Microwave-Receiver-Large
+		type = "item",
+		name = "microwave-receiver-large",
+		icon = icon_mw_rx_2,
+		icon_size = 32,
+		place_as_equipment_result = "microwave-receiver-large",
+		subgroup = "equipment",
+		order = "a[energy-source]-a[solar-panel]-c",
+		stack_size = 50,
+	},
+	{	-- Microwave-Receiver-Biggus-Dickus
+		type = "item",
+		name = "microwave-receiver-biggus-dickus",
+		icon = icon_mw_rx_3,
+		icon_size = 32,
+		place_as_equipment_result = "microwave-receiver-biggus-dickus",
+		subgroup = "equipment",
+		order = "a[energy-source]-a[solar-panel]-d",
+		stack_size = 50,
+	}
 })
-
